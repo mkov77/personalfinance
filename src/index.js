61691441750786c3655c1e6bdc25cd6905308ed5
mkov77 <kovalsky.michaela@gmail.com>
2024-06-29 13:29:47 -0600

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Homepage from './pages/dashboard.tsx';
 import reportWebVitals from './reportWebVitals';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MantineProvider>
+    <React.StrictMode>
+      <Homepage />
+    </React.StrictMode>
+  </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
